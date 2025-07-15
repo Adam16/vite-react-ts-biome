cd3fbed2367ed11f174f132b9e9e6b146ce8359d
Adam Balachowski <adam.balachowski@bertelsmann.de>
2025-07-15 12:39:57 +0200

    feat: adds vitest

---
+++ src/__test__/example.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+
+test("expect.soft test", () => {
+	expect.soft(1 + 1).toBe(2); // mark the test as fail and continue
+	expect.soft(1 + 2).toBe(3); // mark the test as fail and continue
+});
+// reporter will report both errors at the end of the run
