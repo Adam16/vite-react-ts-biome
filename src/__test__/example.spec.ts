78ceb74b1e9327c8e91c9f2ca4f1c58f972d4e4d
Adam Balachowski <adam.balachowski@bertelsmann.de>
2025-07-15 16:28:17 +0200

    feat: adds react testing library

--- src/__test__/example.spec.ts
+++ src/__test__/example.spec.ts
@@ -1,5 +1,3 @@
-import { expect, test } from "vitest";
-
 test("expect.soft test", () => {
 	expect.soft(1 + 1).toBe(2); // mark the test as fail and continue
 	expect.soft(1 + 2).toBe(3); // mark the test as fail and continue
